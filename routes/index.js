6f2bbf5aa74909f8fab825b79063a10e2695af00
Gabukuro <scatamburlo.gabriel@gmail.com>
2019-08-13 20:55:31 -0300

    Update do projeto

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  var dados = {
+    title: 'Aula Server Side',
+    professor: 'Guilherme',
+  }
+  res.render('index', dados);
 });
 
 module.exports = router;
