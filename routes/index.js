eb0aae0b04c8b89adbf5b133966f6d1cbc768e05
GABRIEL SCATAMBURLO MACHADO <gabriel.machado@catolicasc.edu.br>
2019-08-20 20:22:04 -0300

    Adicionada funcionalidade de visualização de um único post

--- routes/index.js
+++ routes/index.js
@@ -9,4 +9,15 @@ router.get('/', function(req, res, next) {
   res.render('index', {title: 'Blog', posts: posts});
 });
 
+router.get('/post/:postId', function(req, res, next) {
+	var postId = req.params.postId;
+  
+  var posts = postsService.getPosts();
+
+  var post = posts.filter((post) => post.id == postId)[0];
+  
+  res.render('post', { title: post.title, post: post })
+
+});
+
 module.exports = router;
--- services/postsService.js
+++ services/postsService.js
@@ -2,16 +2,25 @@
 var getPosts = function() {
     var posts = [
         {
+            id: 1,
             title: 'Post 1',
             image: 'post1.jpg',
             description: 'Meu primeiro post',
             body: 'Meu primeiro post bla bla'
         },
         {
+            id: 2,
             title: 'Post 2',
             image: 'post2.jpg',
             description: 'Meu segundo post',
             body: 'Meu segundo post bla bla'
+        },
+        {
+            id: 3,
+            title: 'Post 3',
+            image: 'post3.jpg',
+            description: 'Meu terceiro post',
+            body: 'Meu terceiro post'
         }
     ];
 
